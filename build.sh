de0157d4fb272366a87974351880c3be69a17755
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:28:17 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-mkdir -pv ${SHED_FAKEROOT}/etc/bash_completion.d
-install -v -m644 ${SHED_CONTRIBDIR}/bashrc ${SHED_FAKEROOT}/etc/
-install -v -m644 ${SHED_CONTRIBDIR}/profile ${SHED_FAKEROOT}/etc/
-mkdir -v ${SHED_FAKEROOT}/etc/skel
-install -v -m644 ${SHED_CONTRIBDIR}/skel/.bash_logout ${SHED_FAKEROOT}/etc/skel/
-install -v -m644 ${SHED_CONTRIBDIR}/skel/.bash_profile ${SHED_FAKEROOT}/etc/skel/
-install -v -m644 ${SHED_CONTRIBDIR}/skel/.bashrc ${SHED_FAKEROOT}/etc/skel/
-install -v -m644 ${SHED_CONTRIBDIR}/skel/.profile ${SHED_FAKEROOT}/etc/skel/
-mkdir -v ${SHED_FAKEROOT}/etc/profile.d
-install -v -m755 ${SHED_CONTRIBDIR}/profile.d/bash_completion.sh ${SHED_FAKEROOT}/etc/profile.d/
-install -v -m755 ${SHED_CONTRIBDIR}/profile.d/extrapaths.sh ${SHED_FAKEROOT}/etc/profile.d/
-install -v -m755 ${SHED_CONTRIBDIR}/profile.d/i18n.sh ${SHED_FAKEROOT}/etc/profile.d/
-install -v -m755 ${SHED_CONTRIBDIR}/profile.d/readline.sh ${SHED_FAKEROOT}/etc/profile.d/
-install -v -m755 ${SHED_CONTRIBDIR}/profile.d/umask.sh ${SHED_FAKEROOT}/etc/profile.d/
+mkdir -pv ${SHED_FAKE_ROOT}/etc/bash_completion.d
+install -v -m644 ${SHED_PKG_CONTRIB_DIR}/bashrc ${SHED_FAKE_ROOT}/etc/
+install -v -m644 ${SHED_PKG_CONTRIB_DIR}/profile ${SHED_FAKE_ROOT}/etc/
+mkdir -v ${SHED_FAKE_ROOT}/etc/skel
+install -v -m644 ${SHED_PKG_CONTRIB_DIR}/skel/.bash_logout ${SHED_FAKE_ROOT}/etc/skel/
+install -v -m644 ${SHED_PKG_CONTRIB_DIR}/skel/.bash_profile ${SHED_FAKE_ROOT}/etc/skel/
+install -v -m644 ${SHED_PKG_CONTRIB_DIR}/skel/.bashrc ${SHED_FAKE_ROOT}/etc/skel/
+install -v -m644 ${SHED_PKG_CONTRIB_DIR}/skel/.profile ${SHED_FAKE_ROOT}/etc/skel/
+mkdir -v ${SHED_FAKE_ROOT}/etc/profile.d
+install -v -m755 ${SHED_PKG_CONTRIB_DIR}/profile.d/bash_completion.sh ${SHED_FAKE_ROOT}/etc/profile.d/
+install -v -m755 ${SHED_PKG_CONTRIB_DIR}/profile.d/extrapaths.sh ${SHED_FAKE_ROOT}/etc/profile.d/
+install -v -m755 ${SHED_PKG_CONTRIB_DIR}/profile.d/i18n.sh ${SHED_FAKE_ROOT}/etc/profile.d/
+install -v -m755 ${SHED_PKG_CONTRIB_DIR}/profile.d/readline.sh ${SHED_FAKE_ROOT}/etc/profile.d/
+install -v -m755 ${SHED_PKG_CONTRIB_DIR}/profile.d/umask.sh ${SHED_FAKE_ROOT}/etc/profile.d/
